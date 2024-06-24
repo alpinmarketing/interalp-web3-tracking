a2fc8e650ae90a3522fa7298d408545c127a9dba
alpinmarketing <46380236+alpinmarketing@users.noreply.github.com>
2024-06-24 14:26:27 +0200

    Update wb3eventtracking.js
    optimize

--- wb3eventtracking.js
+++ wb3eventtracking.js
@@ -1,40 +1,28 @@
 <script>
-function trackEventMatomo(eventName, eventCategory, eventAction, eventLabel) {
-  var _paq = window._paq = window._paq || [];
-  _paq.push(['trackEvent', eventCategory, eventAction, eventLabel]);
-}
-function trackEventGA4(eventCategory, eventAction, eventLabel) {
-  gtag('event', eventAction, {
-    'event_category': eventCategory,
-    'event_label': eventLabel
-  });
-}
 function trackEvent(eventName, eventCategory, eventAction, eventLabel) {
-  trackEventMatomo(eventName, eventCategory, eventAction, eventLabel);
-  trackEventGA4(eventCategory, eventAction, eventLabel);
+  var _paq = window._paq = window._paq || [];
+  var gtag = window.gtag = window.gtag || [];
+  _paq.push(['trackEvent', eventCategory, eventAction, eventLabel]);
+  gtag('event', eventName, {'event_category': eventCategory, 'event_label': eventLabel});
 }
 window.addEventListener('arrival_departure_date', function() {
-  trackEvent('arrival_departure_date', 'ibe', 'arrival_departure_date', 'interesse');
+  trackEvent('view_item_list', 'ibe', 'arrival_departure_date', 'interesse');
 });
 window.addEventListener('viewed_room', function() {
-  trackEvent('viewed_room', 'ibe', 'viewed_room', 'interesse');
+  trackEvent('select_item', 'ibe', 'viewed_room', 'interesse');
 });
 window.addEventListener('viewed_room_detail', function() {
-  trackEvent('viewed_room_detail', 'ibe', 'viewed_room_detail', 'interesse');
+  trackEvent('view_item', 'ibe', 'viewed_room_detail', 'interesse');
 });
 window.addEventListener('add_rate_to_cart', function() {
-  trackEvent('add_rate_to_cart', 'ibe', 'add_rate_to_cart', 'interesse');
+  trackEvent('add_to_cart', 'ibe', 'add_rate_to_cart', 'interesse');
 });
 window.addEventListener('conversion', function(e) {
   trackEvent('conversion', 'ibe', 'buchung', 'buchung');
   var _paq = window._paq = window._paq || [];
+  var gtag = window.gtag = window.gtag || [];
   _paq.push(['trackEcommerceOrder', e.detail.transaction_id, e.detail.value, null, null, null, false]);
-  
-  gtag('event', 'purchase', {
-    'transaction_id': e.detail.transaction_id,
-    'value': e.detail.value,
-    'currency': 'EUR'
-  });
+  gtag('event', 'purchase', {'transaction_id': e.detail.transaction_id,'value': e.detail.value,'currency': 'EUR'});
 });
 </script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-XXXXX"></script>
